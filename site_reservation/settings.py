b06a60679ce19662af8c7c257f54586e31d2d4a6
Virgil Pro <virgil.mesle@sirius-space.fr>
2026-04-18 11:52:59 +0200

    wip

--- site_reservation/settings.py
+++ site_reservation/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
